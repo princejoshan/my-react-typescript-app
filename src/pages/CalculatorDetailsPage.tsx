5bf915abdb7b404405200b0b9f16991735c9ba8b
prince-342 <prince.s@marketsimplified.com>
2024-07-07 23:40:08 +0530

    card navigation added

--- src/pages/CalculatorDetailsPage.tsx
+++ src/pages/CalculatorDetailsPage.tsx
@@ -1,14 +1,21 @@
-import { constants } from "buffer";
-import "../Header.css";
-import { FaqConstants } from "../utility/Constants";
-const CalCulatorDetail = () => {
+
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { CalculatorConstants } from '../utility/Constants';
+
+const CalCulatorDetail: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const card = CalculatorConstants.calculator_list[parseInt(id ?? '0')];
+
+  if (!card) {
+    return <p>Card not found</p>;
+  }
+
   return (
     <div>
-      <div className="header">
-        <div className="left">
-          <div className="title">{"Freqently ask Questions"}</div>
-        </div>
-      </div>
+      <h1>{card.title}</h1>
+      <img src={card.image ?? ""} alt={card.title} />
+      <p>{card.description}</p>
     </div>
   );
 };
